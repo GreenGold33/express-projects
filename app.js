a811a8258d30826d3adc67d1262236bc0487bd28
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-02-24 13:31:36 +0100

    edit and completed

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.locals.moment = require('moment');
 app.set('view engine', 'pug')
 app.use( express.static('public') )
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use( bodyParser.urlencoded({ extended: false }) )
+app.use( bodyParser.json() )
 
 let tasks = [
   {
@@ -23,20 +23,37 @@ let tasks = [
     done: false,
     createdAt: 1487925853757
   },
-   {
+  {
     id: 3,
     title: 'Buy Books',
     done: false,
     createdAt: 1487928442943
+  },
+  {
+    id: 4,
+    title: 'Buy Food',
+    done: true,
+    createdAt: 1487928442943,
+    completedAt: 1487932784319
   }
 ]
 
 app.get('/', (req,res) => {
+  const section = 'home'
   const title = 'TODO List'
-  res.render('home', { title, tasks })
+  const pendingTasks = tasks.filter( task => !task.done )
+  res.render('home', { section, title, tasks: pendingTasks })
 })
 
 app.get('/tasks', (req,res) => res.json(tasks) )
+
+app.get('/tasks/completed', (req,res) => {
+  const section = 'completed'
+  const title = 'COMPLETED tasks'
+  const doneTasks = tasks.filter( task => task.done )
+  res.render('completed', { section, title, tasks: doneTasks })
+})
+
 
 app.post('/tasks', (req,res) => {
   const title = req.body.title;
@@ -56,6 +73,32 @@ app.delete('/task/:id', (req,res) => {
   const id = +req.params.id
   tasks = tasks.filter( task => task.id !== id )
   res.status(200).send(`task ${id} was removed properly`);
+})
+
+app.put('/task/:id', (req,res) => {
+
+  const id = +req.params.id
+  const done = req.body.done === "true" ? true : false
+  const title = req.body.title
+
+  const extra = req.body.extra
+  console.log (extra)
+
+  tasks = tasks.map( task => {
+    if (task.id === id) {
+      if (done) {
+        task.done = done
+        task.completedAt = +new Date()
+      }
+      if (title) {
+        task.title = title
+        task.modifiedAt = +new Date()
+      }
+    }
+    return task
+  })
+  // console.log (tasks)
+  res.status(200).send(`YOUR task ${id} has been properly updated`)
 })
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,39 @@ $('.delete').on('click', function(event) {
   $.ajax({ url, method })
     .done( msg => {
       alert(msg)
-      window.location = '/'
+      // window.location = '/'
+      $(this).closest('li').remove()
     })
+})
+
+$('.done').on('click', function(event) {
+  event.preventDefault()
+  const url = this.href
+  const method = 'PUT'
+  const data = "done=true&extra=this is an extra data"
+  $.ajax({ url, method, data })
+    .done( msg => {
+      alert(msg)
+      $(this).closest('li').remove()
+    })
+})
+
+$('.edit-title').on('click', function(event) {
+  $(this).closest("li").addClass("edit")
+})
+
+$('.pending input').on('keydown', function(event) {
+  if (event.keyCode == 13) {
+    const url = $(this).siblings("a").attr("href")
+    const method = 'PUT'
+    const newTitle = $(this).val()
+    const data = 'title=' + newTitle
+    $.ajax({ url, method, data })
+      .done( msg => {
+        $(this).siblings("span").text(newTitle)
+        $(this).closest('li').removeClass("edit")
+      })
+
+  }
+
 })
